ac17f6cab314b5df18d79743268c4f70b59542dd
Paul Brook <pjbubbs@live.co.uk>
2025-03-19 20:14:35 +0000

    take out protected route

--- SplitMedia.ui/src/routes/Routes.tsx
+++ SplitMedia.ui/src/routes/Routes.tsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import SignUp from "../Pages/marketing/signUp/SignUp";
+import SignUpThankYou from "../Pages/marketing/signUpThankYou/SignUpThankYou";
 import Root from "../Pages/root/root";
 import ErrorPage from "../error-page";
 import AboutUs from "../Pages/aboutUs";
@@ -7,22 +9,11 @@ import PrivacyPolicy from "../Pages/Terms/privacyPolicy";
 import CookiePolicy from "../Pages/Terms/cookiePolicy";
 import Register from "../Pages/User/registration/register/register";
 import LearnMore from "../Pages/marketing/learnMore";
-import MyAccount from "../Pages/User/myAccount";
-import ChangePassword from "../Pages/User/changePassword";
 import Pricing from "../Pages/marketing/pricing";
-import AddGallery from "../Pages/Gallery/addGallery";
-import ViewGallery from "../Pages/Gallery/viewGallery/viewGallery";
 import Login from "../Pages/User/login";
 import SelectPlan from "../Pages/User/registration/selectPlan/selectPlan";
-import Payment from "../Pages/User/registration/payment/Payment";
-import TakePayment from "../Pages/User/registration/takePayment/TakePayment";
 import ThankYou from "../Pages/User/registration/thankYou/ThankYou";
-import ProtectedRoute from "./ProtectedRoute";
-import DashBoard from "../Pages/User/dashboard/dashBoard";
-import ViewGuestGallery from "../Pages/Gallery/guestGallery/viewGuestGallery";
 import Logout from "../Pages/User/logout";
-import SignUp from "../Pages/marketing/signUp/SignUp";
-import SignUpThankYou from "../Pages/marketing/signUpThankYou/SignUpThankYou";
 
 export const router = createBrowserRouter([
   {
@@ -48,64 +39,4 @@ export const router = createBrowserRouter([
   { path: "privacy-policy", element: <PrivacyPolicy /> },
   { path: "learn-more", element: <LearnMore /> },
   { path: "pricing", element: <Pricing /> },
-  {
-    path: "my-account",
-    element: (
-      <ProtectedRoute>
-        <MyAccount />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "change-password",
-    element: (
-      <ProtectedRoute>
-        <ChangePassword />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "dashboard",
-    element: (
-      <ProtectedRoute>
-        <DashBoard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "add-gallery",
-    element: (
-      <ProtectedRoute>
-        <AddGallery />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "gallery/:id",
-    element: (
-      <ProtectedRoute>
-        <ViewGallery />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "guestgallery/:gid/:sid",
-    element: <ViewGuestGallery />,
-  },
-  {
-    path: "payment",
-    element: (
-      <ProtectedRoute>
-        <Payment />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "take-payment",
-    element: (
-      <ProtectedRoute>
-        <TakePayment />
-      </ProtectedRoute>
-    ),
-  },
 ]);
